cb57621878aae8620d9f6201ee859b6dc0226a21
davex-ai <omotoyedave4@gmail.com>
2026-01-23 01:34:09 +0100

    ....

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import IntroScreen from './app/screens/IntroScreen';
+import SvgLogo from '././components/SvgLogo';
+import PageTwo from './app/screens/PageTwo';
+import PageThree from './app/screens/PageThree';
+import Payment from './app/screens/Payment';
+import SecondIntroScreen from './app/screens/Second IntroScreen';
+import GetStarted from './app/screens/GetStarted';
+import Shop from './app/screens/Shop';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <Shop />  */}
+      {/* <IntroScreen/> */}
+      {/* <PageThree/> */}
+      {/* <PageTwo/> */}
+      {/* <Payment/> */}
+      {/* <SecondIntroScreen/> */}
+      <GetStarted/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- app/screens/IntroScreen.tsx
+++ app/screens/IntroScreen.tsx
@@ -1,6 +1,6 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import SvgLogo from './../../components/SvgLogo';
+import SvgLogo from '../../components/SvgLogo';
 
 const IntroScreen = () => {
   return (
